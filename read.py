8e8366d79a3cdfcf6adb5b225e12be9db59bcf0c
NJ Kuan <cannokuan@gmail.com>
2020-01-13 23:18:31 +0800

    calc abg length

--- read.py
+++ read.py
@@ -9,8 +9,10 @@ with open('reviews.txt', 'r') as file:
 		if count % 1000 == 0: # 1000的倍數
 			print (len(data))
 
-print (len(data))
+print ('檔案讀取完了, 總共有', len(data), '筆資料')
 
-print (data[0])
-print ('--------------')
-print (data[1])
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+
+print ('每筆留言平均長度是', sum_len/len(data))
